a4a5665ccd7b13296173186da3bfed87fa62f982
Michael Roberts <mikeroberts421@gmail.com>
2023-04-04 13:05:17 -0600

    adding icons and starting formatting

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,68 @@
 const defaultLocale = `Boise, ID, USA`;
 
+const getCorrectIcon = (data) => {
+    const description = data.shortForecast.toLowerCase();
+    if (description.includes("sunny")) return `./../imgs/icons/128/day_clear.png`;
+    if (description.includes("cloudy")) return `./../imgs/icons/128/cloudy.png`;
+    if (description.includes("thunder")) return `./../imgs/icons/128/thunder.png`;
+    if (description.includes("rain")) return `./../imgs/icons/128/rain.png`;
+    if (description.includes("snow")) return `./../imgs/icons/128/snow.png`;
+    if (description.includes("sleet")) return `./../imgs/icons/128/sleet.png`;
+    if (description.includes("wind")) return `./../imgs/icons/128/wind.png`;
+    return `./../imgs/icons/128/overcast.png`
+};
+
+const buildInnerCard = (data, type) => {
+    return `
+        <div class="wa__card--inner wa__card--conditions">
+            <h6>${data.name}</h6>
+            <div class="wa__card--row">
+                <h4 class="symbol--degree">${data.temperature}</h4>
+                <img data-icon src="${getCorrectIcon(data)}" alt="Current Conditions Icon">
+            </div>
+            <h5>${(type && type === "long") ? data.detailedForecast : data.shortForecast}</h5>
+        </div>
+    `;
+};
+
+const buildTopCard = (data) => {
+    return `
+        <div class="wa__card--shadow">
+            <div class="wa__card--inner wa__card--conditions">
+                ${buildInnerCard(data)}
+            </div>
+            <div class="wa__card--vertical-line"></div>
+            <div class="wa__card--inner wa__card--details">
+                <p class="wa__card--details-text">
+                    <span>${new Date().toDateString()}</span>
+                </p>
+                <p class="wa__card--details-text">
+                    Wind Speed: <span>${data.windSpeed}</span>
+                </p>
+                <p class="wa__card--details-text">
+                    Wind Direction: <span>${data.windDirection}</span>
+                </p>
+                <p class="wa__card--details-text">
+                    Humidity: <span>${data.relativeHumidity.value}%</span>
+                </p>
+                <p class="wa__card--details-text">
+                    Chance of Percipitation: <span>${data.probabilityOfPrecipitation.value}%</span>
+                </p>
+            </div>
+        </div>
+    `;
+};
+
+const buildSlide = (data) => {
+    return `
+        <li class="glide__slide wa__card">
+            <div class="wa__card--shadow">
+                ${buildInnerCard(data)}
+            </div>
+        </li>
+    `;
+};
+
 const getForecastCall = async (locale) => {
     return await fetch(`http://localhost:3001/calls/getForecast?locale=${locale}`, {
         method: "GET"
@@ -30,9 +93,33 @@ const setCurrentLocationBox = (locale) => {
     document.querySelector("[data-current-location]").innerHTML = locale;
 }
 
-const setUpCarousel = (forecastData) => {
+
+
+const setUpTopCard = (forecast) => {
+    if (!forecast) return null;
+    const topCardTitle = document.querySelector("[data-current-title]");
+    topCardTitle.textContent = forecast.name ? forecast.name : ``;
+    const topCardContainer = document.querySelector("[data-card-large]");
+    const topCardBuilt = buildTopCard(forecast);
+    topCardContainer.insertAdjacentHTML('beforeend', topCardBuilt);
+};
+
+const setUpCarousel = (forecast) => {
+    if (!forecast) return null;
+
+    const carouselContainer = document.querySelector("[data-carousel-container]");
+    forecast.forEach(period => {
+        const slide = buildSlide(period);
+        carouselContainer.insertAdjacentHTML('beforeend', slide);
+    });
     new Glide('.glide', {
         perView: 2,
+        peek: { before: 0, after: 50 },
+        breakpoints: {
+            800: {
+                perView: 1
+            }
+        }
     }).mount();
 };
 
@@ -46,8 +133,11 @@ const setPageData = (forecastData) => {
 
     setCurrentLocationBox(firstLocaleResult.formatted_address ? firstLocaleResult.formatted_address : null);
 
-    setUpCarousel(forecastData);
-    console.log("forecastData", forecastData.forecast.properties.periods);
+    if (forecastData?.forecast?.properties?.periods.length > 1) {
+        setUpTopCard(forecastData.forecast.properties.periods[0])
+        setUpCarousel(forecastData.forecast.properties.periods.slice(1));
+    }
+
 };
 
 const getAndSetAllForecastData = async (locale = defaultLocale) => {
